000c281018e05249d390a1190af320d24ca8e0a9
Phattharanit (Phatt) <121805649+phattharanit-pewklieng@users.noreply.github.com>
2023-03-17 13:23:16 +1300

    Update App.tsx

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,27 @@
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
+     <div className="container">
+      <div className="header">
+        <h1>App</h1>
+
+        <div className="center">
+          <form className="text-black flex flex-col">
+            <label htmlFor="title">Type Here</label>
+            <input type="text" name="title" id="title" />
+          </form>
+        </div>
+        <div className="center">
+          <button className="button" type="submit">
+            Click me
+          </button>
+        </div>
+
+        <p>What your idea!</p>
+      </div>
+
+      <div className="box">
+        <p>Hi</p>
+      </div>
     </div>
   )
 }
