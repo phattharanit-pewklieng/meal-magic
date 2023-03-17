85857c59834c317ec6af7bc7f0ea4c194b86828e
phattharanit-pewklieng <phattharanit.noei@gmail.com>
2023-03-17 13:38:48 +1300

    add h1

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,28 @@
 function App() {
   return (
+     <div className="container">
+      <div className="header">
+        <h1>App</h1>
 
+        <div className="center">
+          <form className="text-black flex flex-col">
+            <label htmlFor="title">Type Here</label>
+            <input type="text" name="title" id="title" />
+          </form>
+        </div>
+        <div className="center">
+          <button className="button" type="submit">
+            Click me
+          </button>
+        </div>
 
+        <p>What your idea!</p>
+      </div>
+
+      <div className="box">
+        <p>Hi</p>
+      </div>
+    </div>
   )
 }
 
